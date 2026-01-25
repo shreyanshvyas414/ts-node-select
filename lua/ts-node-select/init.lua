b15aa1a2c1ab614c81e38c730830387670a0b422
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2026-01-25 23:54:01 +0530

    feat: implement keymap wiring with customizable options

--- lua/ts-node-select/init.lua
+++ lua/ts-node-select/init.lua
@@ -2,10 +2,23 @@
 
 local M = {}
 
+local core = require("ts-node-select.core")
+local keymaps = require("ts-node-select.keymaps")
+local selection = require("ts-node-select.selection")
+
+
 function M.setup(opts)
   opts = opts or {}
-  -- internals will be wired later
+  -- Set up the core (Treesitter starup)
+  core.setup()
+
+  -- Set up keymaps or (pass options)
+  keymaps.setup(opts.keymaps)
 end
+
+M.init = selection.init
+M.expand = selecton.expand
+M.shrink = selection.shrink
 
 return M
 
