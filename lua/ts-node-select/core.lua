013f490f4bb6c91a8f67eb940b2701cc467d3b72
Shreyansh Vyas <vyasshreyansh71@gmail.com>
2026-01-25 22:50:39 +0530

    feat: add safe Treesitter startup using new API

---
+++ lua/ts-node-select/core.lua
@@ -0,0 +1,23 @@
+--  lua/ts-node-select/core.lua.
+
+-- Prevent buffer crashes (oil, help, nofile, etc).
+-- Uses new API only.
+-- No dependency on nvim-treesitter.configs.
+-- Treesitter starts only when safe.
+
+local M = {}
+
+function M.setup()
+  vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+    local ft = vim.bo.filetype
+    local lang = vim.treesitter.language.get_lang(ft)
+
+    if not lang then return end
+
+    -- Only start treesitter if a parser actually exists
+    if pcall(vim.treesitter.language.add, lang) then
+      pcall(vim.treesitter.start)
+    end
+  })
+end
